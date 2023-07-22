0dcfeba0f571e92814defbc22d079bbf6ba8a4cb
Weiyun Lu <weiyun.lu@gmail.com>
2023-07-22 08:33:55 -0400

    Rest of controller tests

--- test/controllers/api/pokemon_controller_test.rb
+++ test/controllers/api/pokemon_controller_test.rb
@@ -18,8 +18,33 @@ class Api::PokemonControllerTest < ActionDispatch::IntegrationTest
       legendary: false
     }
   end
+
+  def create_twenty_missingnos
+    (0..19).each do |i|
+      Pokemon.create(pokemon_params(999, i, "Missingno Alt#{i}"))
+    end
+  end
   
   # index
+  test '#index returns paginated list with default limit of 10, offset of 0' do
+    create_twenty_missingnos
+
+    get '/api/pokemon'
+
+    parsed_response = JSON.parse(response.body)
+    assert_equal 10, parsed_response.size
+    assert_equal 0, parsed_response.first['form_id']
+  end
+
+  test '#index returns limited/offset paginated list when params passed' do
+    create_twenty_missingnos
+
+    get '/api/pokemon', params: { limit: 5, offset: 10 }
+
+    parsed_response = JSON.parse(response.body)
+    assert_equal 5, parsed_response.size
+    assert_equal 10, parsed_response.first['form_id']
+  end
 
   # show
   test '#show finds pokemon with default form when form_id not passed' do
@@ -47,24 +72,81 @@ class Api::PokemonControllerTest < ActionDispatch::IntegrationTest
   end
 
   # create
-  test '#create creates pokemon with 201 status when successful' do
-    
+  test '#create creates pokemon and returns entity with 201 status when successful' do
+    post '/api/pokemon', params: { pokemon: pokemon_params }
+    assert_response 201
+
+    assert_equal 1, Pokemon.count
+    assert_equal response.body, Pokemon.first.entity.to_json
   end
 
   test '#create returns 400 error when pokemon could not be created' do
+    post '/api/pokemon', params: { pokemon: pokemon_params.merge(hp: nil) }
+    assert_response 400
+
+    assert_equal 0, Pokemon.count
   end
 
   # update
-  test '#update updates pokemon with 200, only on provided parameters, when successful' do
+  test '#update updates pokemon with 200, when successful' do
+    pokemon = Pokemon.create(pokemon_params)
+    put '/api/pokemon/999', params: { pokemon: { hp: 120, attack: 120 } }
+
+    pokemon.reload
+    assert_equal 120, pokemon.hp
+    assert_equal 120, pokemon.attack
+    assert_equal pokemon_params[:defense], pokemon.defense
+    assert_equal pokemon_params[:speed], pokemon.speed
+
+    assert_response 200
+  end
+
+  test '#update updates pokemon with alternate form with 200, when successful' do
+    pokemon = Pokemon.create(pokemon_params)
+    pokemon2 = Pokemon.create(pokemon_params(999, 1, 'MissingnoAlt'))
+
+    put '/api/pokemon/999', params: { form_id: 1, pokemon: { hp: 120, attack: 120 } }
+
+    pokemon.reload
+    pokemon2.reload
+
+    assert_equal 100, pokemon.hp
+    assert_equal 100, pokemon.attack
+    assert_equal 120, pokemon2.hp
+    assert_equal 120, pokemon2.attack
+
+    assert_response 200
   end
 
   test '#update returns 400 error when update unsuccessful' do
+    pokemon = Pokemon.create(pokemon_params)
+    pokemon2 = Pokemon.create(pokemon_params(999, 1, 'MissingnoAlt'))
+
+    put '/api/pokemon/999', params: { pokemon: { form_id: 1 }}
+    assert_response 400
   end
 
   # destroy
   test '#destroy destroys pokemon with 200, when found' do
+    pokemon = Pokemon.create(pokemon_params)
+
+    delete '/api/pokemon/999'
+    assert_equal 0, Pokemon.count
+    assert_response 200
+  end
+
+  test '#destroy destroys pokemon with alternate form with 200, when found' do
+    pokemon = Pokemon.create(pokemon_params)
+    pokemon2 = Pokemon.create(pokemon_params(999, 1, 'MissingnoAlt'))
+
+    delete '/api/pokemon/999', params: { form_id: 1 }
+    assert_equal 1, Pokemon.count
+    assert_equal 0, Pokemon.first.form_id
+    assert_response 200
   end
 
   test '#destroy returns 404 error when pokemon cannot be found' do
+    delete '/api/pokemon/1'
+    assert_response 404
   end
 end
